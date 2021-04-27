c431db1a4e835e91f243457ef9f07e425226a170
hiepvo01 <vohi0101@luther.edu>
2021-04-27 09:29:56 -0500

    only template

---
+++ form/script.js
@@ -0,0 +1,137 @@
+var cards_dict = {};
+
+$(document).ready(function() {
+    $(".quantity").change(function() {
+        var $row = $(this).closest("tr");
+        var special = "";
+        $row.children().each(function(idx, val){
+            if(val.innerHTML.includes('special1')) {
+                special = "special1"
+            } else if(val.innerHTML.includes('special2')) {
+                special = "special2"
+            } else if(val.innerHTML.includes('special3')) {
+                special = "special3"
+            };
+        })    
+        
+        if (special == ""){
+            var $text = $row.find(".denomination").text();
+            var $amount = $row.find(".amount")
+            
+            var label = $(this).closest('tr').prevAll('tr:has(td.merchant):first')
+                     .children('td.merchant');
+
+            if (!(label.text() in cards_dict)){
+                cards_dict[label.text()] = [{
+                    'denomination': $text,
+                    'quantity': $(this).val(),
+                    'amount': math.evaluate($text + '*' + $(this).val()),
+                    'discretion': math.evaluate($text + '*' + $(this).val() + '*0.01'),
+                }];
+            } else {
+                let exist = false;
+                for (c of cards_dict[label.text()]) {
+                    if(c['denomination'] == $text){
+                        c['quantity'] = $(this).val();
+                        c['amount'] = math.evaluate($text + '*' + $(this).val());
+                        c['discretion'] = math.evaluate($text + '*' + $(this).val() + '*0.01')
+                        exist = true;
+                    }
+                }
+                if(!exist) {
+                    cards_dict[label.text()].push({
+                        'denomination': $text,
+                        'quantity': $(this).val(),
+                        'amount': math.evaluate($text + '*' + $(this).val()),
+                        'discretion': math.evaluate($text + '*' + $(this).val() + '*0.01')
+                    })
+                }
+            }
+
+            $amount.text(math.evaluate($text + '*' + $(this).val()))
+        } else {
+            var $text = document.getElementById(special).value
+            var $amount = $row.find(".amount")
+            
+            $amount.text(math.evaluate($text + '*' + $(this).val()))
+        }
+        var due = 0
+        $('.amount').each(function(idx, val){
+            var totalDue = document.getElementById("totalDue")
+            if (parseFloat(val.innerHTML) > 0){
+                due += parseFloat(val.innerHTML)
+                totalDue.innerText= due;
+            }
+        })
+        
+        inputs = document.getElementsByClassName('quantity');
+        let cards = 0
+        for (index = 0; index < inputs.length; ++index) {
+            var totalCards = document.getElementById("totalCards")
+            if (parseFloat(inputs[index].value) > 0){
+                cards += parseFloat(inputs[index].value);
+                totalCards.innerText= cards;
+            }
+        }
+
+        var totalDiscretion = document.getElementById("totalDiscretion");
+        totalDiscretion.innerText= due * 1 / 100;
+    });
+  })
+
+  function ckChange(ckType){
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      } 
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      } 
+    }    
+}
+
+function printPage(){
+    window.print(cards_dict);
+}
+
+function submitForm(){
+    var request = new XMLHttpRequest();
+    request.open('POST', 'http://127.0.0.1:5000/post_orders');
+    request.setRequestHeader("Access-Control-Allow-Origin", "*");
+    request.setRequestHeader("Content-Type", "application/json");
+    
+    let target = "";
+    let purchaser = document.getElementById("purchaser");
+    let date = document.getElementById("date");
+    if(purchaser.value == "" || date.value == ""){
+        alert("Purchaser or Date not Entered")
+    }
+    if(document.getElementById('student').checked) {
+        studentName = document.getElementById('studentName').value
+        target = "Student " + studentName
+    } else if (document.getElementById('students').checked) {
+        target = "Student Fund"
+    } else if (document.getElementById('generalFund').checked) {
+        target = "General Fund"
+    } else {
+        alert("Purchaser Discretion not selected")
+    }
+    if(target != "" && purchaser.value != "" && date.value != ""){
+        cards_dict['Target'] = target;
+        cards_dict['Purchaser'] = purchaser.value;
+        cards_dict['Date'] = date.value;
+        let newdict = JSON.stringify(cards_dict);
+        request.send(newdict);
+        alert("Cards Data submitted")
+    }
+}
---
+++ manager/script.js
@@ -0,0 +1,166 @@
+window.onload = async function() {
+    let students = await fetch("http://vohi01.pythonanywhere.com//students")
+    .then(response => response.json())
+
+    var select = document.getElementById("selectStudent");
+    students = students['students']
+    for(let i = 0; i < students.length; i++) {
+        let opt = students[i];
+        let el = document.createElement("option");
+        el.innerText = opt;
+        select.appendChild(el);
+    }
+
+    
+    var select = document.getElementById("selectFund");
+    students = ['General Fund', 'Student Fund']
+    for(let i = 0; i < students.length; i++) {
+        let opt = students[i];
+        let el = document.createElement("option");
+        el.innerText = opt;
+        select.appendChild(el);
+    }
+
+    let merchants = await fetch("http://vohi01.pythonanywhere.com//merchants")
+    .then(response => response.json())
+    
+    var select = document.getElementById("selectMerchant");
+    merchants = merchants['merchants']
+    for(let i = 0; i < merchants.length; i++) {
+        let opt = merchants[i];
+        let el = document.createElement("option");
+        el.innerText = opt;
+        select.appendChild(el);
+    }
+}
+
+async function getStudent(){
+    student = document.getElementById('selectStudent').value
+    studentName = student.split(" ")[1]
+    url = "http://vohi01.pythonanywhere.com//students/" + studentName
+    let result = await fetch(url).then(response => response.json())
+
+    let studentTotal = document.getElementById('studentTotal');
+    let total = result[result.length-1]['Total Discretion']
+    studentTotal.innerHTML = "The total amount donated to student " + studentName + " is: $" + total
+
+    var tbody = document.getElementById('sbody');
+    tbody.innerHTML = "";
+    for (var i = 0; i < result.length -1; i++) {
+        let row1 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let thead = document.createElement('th');
+            thead.innerHTML = k;
+            row1.appendChild(thead)
+        }
+        tbody.appendChild(row1)
+        let row2 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let tdata = document.createElement('td');
+            tdata.innerHTML = result[i][k];
+            row2.appendChild(tdata)
+        }
+        tbody.appendChild(row2)
+    }
+}
+
+async function getFund(){
+    let oldfund = document.getElementById('selectFund').value;
+    fund = oldfund.replace(' ', '');
+    fund = fund.toLowerCase();
+    url = "http://vohi01.pythonanywhere.com//" + fund
+    let result = await fetch(url).then(response => response.json())
+
+    let studentTotal = document.getElementById('fundTotal');
+    let total = result[result.length-1]['Total Discretion']
+    studentTotal.innerHTML = "The total amount donated to " + oldfund + " is: $" + total
+
+    var tbody = document.getElementById('fbody');
+    tbody.innerHTML = "";
+    for (var i = 0; i < result.length -1; i++) {
+        let row1 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let thead = document.createElement('th');
+            thead.innerHTML = k;
+            row1.appendChild(thead)
+        }
+        tbody.appendChild(row1)
+        let row2 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let tdata = document.createElement('td');
+            tdata.innerHTML = result[i][k];
+            row2.appendChild(tdata)
+        }
+        tbody.appendChild(row2)
+    }
+}
+
+async function getMerchant(){
+    let oldfund = document.getElementById('selectMerchant').value;
+    if (oldfund.includes("A&W/LIS 10%")){
+        oldfund= "A&W_LIS10%"
+    }
+    fund = oldfund.replace(' ', '%20');
+    url = "http://vohi01.pythonanywhere.com//merchants/" + fund
+    let result = await fetch(url).then(response => response.json())
+
+    let studentTotal = document.getElementById('merchantTotal');
+    console.log(result[result.length-1])
+    let total = result[result.length-1]['Total Cards']
+    studentTotal.innerHTML = "The total number of cards sold by " + oldfund + " is: " + total
+
+    var tbody = document.getElementById('mbody');
+    tbody.innerHTML = "";
+    for (var i = 0; i < result.length -1; i++) {
+        let row1 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let thead = document.createElement('th');
+            thead.innerHTML = k;
+            row1.appendChild(thead)
+        }
+        tbody.appendChild(row1)
+        let row2 = document.createElement('tr')
+        for (var k of Object.keys(result[i])) {
+            let tdata = document.createElement('td');
+            tdata.innerHTML = result[i][k];
+            row2.appendChild(tdata)
+        }
+        tbody.appendChild(row2)
+    }
+}
+
+async function date(){
+    bdate = document.getElementById('beginDate').value;
+    edate = document.getElementById('endDate').value
+    console.log(bdate)
+    if(bdate =="" || edate =="") {
+        alert("Please select both begin date and end date")
+    } else {
+        url = "http://vohi01.pythonanywhere.com//date/" + bdate + '/' + edate
+        let result = await fetch(url).then(response => response.json())
+
+        let studentTotal = document.getElementById('dateTotal');
+        console.log(result[result.length-1])
+        let total = result[result.length-1]['Total Cards']
+        studentTotal.innerHTML = "The number of cards sold between " + bdate + ' and ' + edate + " is: " + total
+
+        var tbody = document.getElementById('dbody');
+        tbody.innerHTML = "";
+        for (var i = 0; i < result.length -1; i++) {
+            let row1 = document.createElement('tr')
+            for (var k of Object.keys(result[i])) {
+                let thead = document.createElement('th');
+                thead.innerHTML = k;
+                row1.appendChild(thead)
+            }
+            tbody.appendChild(row1)
+            let row2 = document.createElement('tr')
+            for (var k of Object.keys(result[i])) {
+                let tdata = document.createElement('td');
+                tdata.innerHTML = result[i][k];
+                row2.appendChild(tdata)
+            }
+            tbody.appendChild(row2)
+        }
+    }
+}
